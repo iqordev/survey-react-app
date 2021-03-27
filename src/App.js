d1d53c7a22eb8bac1af38a81920a3e3bc64bc4b8
Kevin Tuazon <kevintuazon45@gmail.com>
2021-03-27 11:50:38 +0800

    deploy to  heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import "survey-react/survey.css";
+import * as Survey from "survey-react";
 
 function App() {
+  const [surveyJSON, setsurveyJSON] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener("message", function (data) {
+      console.log(JSON.parse(data.data));
+      setsurveyJSON(JSON.parse(data.data));
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Survey.Survey
+        json={JSON.stringify(surveyJSON)}
+        onComplete={() => console.log("submit")}
+      />
     </div>
   );
 }
